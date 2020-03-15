3dc4a2207201549283192648544dfdd52f4f486f
Bruce Lee <conpheedence@gmail.com>
2020-03-16 01:03:08 +0800

    Submit pro-22

--- 22-GenerateParentheses/v2.cpp
+++ 22-GenerateParentheses/v2.cpp
@@ -2,13 +2,6 @@
 #include <vector>
 using namespace std;
 
-struct treeNode {
-    char val;
-    treeNode* lptr = NULL;
-    treeNode* rptr = NULL;
-    treeNode(char add) : val(add) {}
-};
-
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
@@ -16,23 +9,20 @@ public:
         if(n == 0) return *result;
         int left = n - 1; //剩余可以使用的左括号
         int right = n; //可以使用的右括号
-        treeNode* start = new treeNode('(');
-        string temp = "";
-        reachKid(start, temp, left, result, right);
+        string temp = "(";
+        reachKid(temp, left, result, right);
         return *result;
     }
 
-    void reachKid(treeNode* cur, string pre, int left, vector<string> *result, int right) {
+    void reachKid(string pre, int left, vector<string> *result, int right) {
         if(left == 0 && right == 0) { //到达子节点
-            result->push_back(pre + cur->val);
+            result->push_back(pre);
         }
         if(left > 0) { //新建左边子节点
-            cur->lptr = new treeNode('(');
-            reachKid(cur->lptr, pre + cur->val, left - 1, result, right);
+            reachKid(pre + '(', left - 1, result, right);
         }
         if(right > left) { //右边子节点
-            cur->rptr = new treeNode(')');
-            reachKid(cur->rptr, pre + cur->val, left, result, right - 1);
+            reachKid(pre + ')', left, result, right - 1);
         }
     }
 };
