6637b1f37ba3b4d5294e9fb2b997c2d77c0c6f52
Bruce Lee <conpheedence@gmail.com>
2020-03-15 22:14:16 +0800

    Modify

--- 22-GenerateParentheses/v1.cpp
+++ 22-GenerateParentheses/v1.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 using namespace std;
 
+struct treeNode {
+    char val;
+    treeNode* lptr = NULL;
+    treeNode* rptr = NULL;
+}
+
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
+        vector<string> result(0);
+        if(n == 0) return result;
+        else if(n == 1) {
+            result.resize(1);
+            result[1] = "()";
+        }
         
+
     }
 };
