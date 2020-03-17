1b39d1395edbd04ce9338f8c17f26ea08b20bbf2
Bruce Lee <conpheedence@gmail.com>
2020-03-17 19:59:32 +0800

    Submit pro-32

--- 32-LongestValidParentheses/v1.cpp
+++ 32-LongestValidParentheses/v1.cpp
@@ -4,6 +4,26 @@ using namespace std;
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        
+        int maxLen = 0;
+        int len = s.size();
+        if(len <= 1) return 0; //特判
+        int dp[len];
+        memset(dp, 0, len*sizeof(int));
+        for(int i = 1; i < len; i++) {
+            if(s[i] == ')') {
+                if(s[i - 1] == '(') dp[i] = (i > 1 ? dp[i - 2] : 0) + 2;
+                else if(i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(')
+                    dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] > 1 ? dp[i - dp[i - 1] - 2] : 0);
+                maxLen = max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
     }
 };
+
+int main()
+{
+    Solution solu;
+    string s;
+    solu.longestValidParentheses(s);
+}
